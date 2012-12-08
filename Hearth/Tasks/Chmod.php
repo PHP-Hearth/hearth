c0c801dfce52032acee1b0bf1acb1aabd056107e
Maxwell Vandervelde <Max@MaxVandervelde.com>
2012-12-08 16:04:54 -0600

    Added setters/getters added file getters and setters in the chmod task

--- Hearth/Tasks/Chmod.php
+++ Hearth/Tasks/Chmod.php
@@ -5,6 +5,7 @@
  * Description of Chmod.php
  * 
  * @category Hearth
+ * @package Targets
  * @package Expression package is undefined on line 8, column 15 in Templates/Scripting/PHPClass.php.
  * @author Maxwell Vandervelde <Maxwell.Vandervelde@nerdery.com>
  */
@@ -17,13 +18,18 @@ namespace Hearth\Tasks;
  * Description of Chmod
  *
  * @category Hearth
+ * @package Targets
  * @package Expression package is undefined on line 18, column 15 in Templates/Scripting/PHPClass.php.
  * @author Maxwell Vandervelde <Maxwell.Vandervelde@nerdery.com>
  * @version $Id$
  */
 class Chmod
 {
-    
+    /**
+     * @var string The file to run the chmod command on
+     */
+    protected $_file;
+
     /**
      * main
      * 
@@ -36,6 +42,49 @@ class Chmod
     {
 
         return;
+    }
+
+    /**
+     * setFile
+     *
+     * Set the file to run the chmod command on
+     *
+     * @access public
+     * @param string $file
+     * @return \Hearth\Tasks\Chmod
+     * @throws \InvalidArgumentException
+     */
+    public function setFile($file)
+    {
+        if (!is_string($file)) {
+            throw new \InvalidArgumentException(
+                'Unexpected ' . gettype($file) . '. Expected a string'
+            );
+        }
+
+        $this->_file = $file;
+
+        return $this;
+    }
+
+    /**
+     * getFile
+     *
+     * Get the file to run the chmod command on
+     *
+     * @access public
+     * @return string
+     * @throws \Exception
+     */
+    public function getFile()
+    {
+        if (!isset($this->_file)) {
+            throw new \Exception(
+                'No File was set to chmod'
+            );
+        }
+
+        return $this->_file;
     }
 
 }
