42320b3392a7b8206ac409052184f1e7cad4de25
Maxwell Vandervelde <Max@MaxVandervelde.com>
2012-12-08 15:51:15 -0600

    Adding base classes

---
+++ Hearth/Targets/DemoTarget.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * DemoTarget.php
+ * 
+ * @category Hearth
+ * @author Maxwell Vandervelde <Maxwell.Vandervelde@nerdery.com>
+ */
+
+namespace Hearth\Target;
+
+/**
+ * DemoTarget
+ *
+ * @category Hearth
+ * @author Maxwell Vandervelde <Maxwell.Vandervelde@nerdery.com>
+ * @version $Id$
+ */
+class DemoTarget
+{
+    
+}
---
+++ Hearth/Tasks/Chmod.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Chmod.php
+ *
+ * Description of Chmod.php
+ * 
+ * @category Hearth
+ * @package Expression package is undefined on line 8, column 15 in Templates/Scripting/PHPClass.php.
+ * @author Maxwell Vandervelde <Maxwell.Vandervelde@nerdery.com>
+ */
+
+namespace Hearth\Tasks;
+
+/**
+ * Chmod
+ *
+ * Description of Chmod
+ *
+ * @category Hearth
+ * @package Expression package is undefined on line 18, column 15 in Templates/Scripting/PHPClass.php.
+ * @author Maxwell Vandervelde <Maxwell.Vandervelde@nerdery.com>
+ * @version $Id$
+ */
+class Chmod
+{
+    
+    /**
+     * main
+     * 
+     * Description of main
+     * 
+     * @access public
+     * @return void
+     */
+    public function main()
+    {
+
+        return;
+    }
+
+}
