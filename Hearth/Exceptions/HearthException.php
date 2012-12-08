deeef8172c0d7794114fb7cbb3d14ec6cf333fc9
Douglas Linsmeyer <dlinsmey@athens.sierrabravo.net>
2012-12-08 17:10:35 -0600

    Added a bunch of exception bullshit

---
+++ Hearth/Exceptions/HearthException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Hearth\Exception;
+
+class HearthException
+{
+
+}
---
+++ Hearth/Exceptions/InvalidTargetLocation.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Hearth\Exception;
+
+class InvalidTargetLocation extends HearthException
+{
+
+}
