fedbd7873b7c9722bfd8d7951676f0f05307d4f2
Maxwell Vandervelde <Max@MaxVandervelde.com>
2012-12-08 20:16:02 -0600

    Adding in tests

---
+++ Hearth/Test/Hearth/CoreTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * CoreTest.php
+ *
+ * Description of CoreTest.php
+ * 
+ * @category Hearth
+ * @package Test
+ * @author Maxwell Vandervelde <Maxwell.Vandervelde@nerdery.com>
+ * @version 0.0.0
+ * @license http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode
+ *          Attribution-NonCommercial-ShareAlike 3.0 Unported
+ *          Some Rights Reserved
+ */
+
+/**
+ * CoreTest
+ *
+ * Description of CoreTest
+ *
+ * @category Hearth
+ * @package Test
+ * @author Maxwell Vandervelde <Maxwell.Vandervelde@nerdery.com>
+ */
+class CoreTest extends PHPUnit_Framework_TestCase
+{
+    public function testNothing()
+    {
+        $this->assertTrue(true);
+    }
+}
