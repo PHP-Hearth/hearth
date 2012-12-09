b21aba76f8e4a5a30f5aef30ae9397df3e0284c0
Maxwell Vandervelde <Max@MaxVandervelde.com>
2012-12-08 18:27:31 -0600

    Added in task registry to Target abstract class

--- Hearth/Target.php
+++ Hearth/Target.php
@@ -25,12 +25,23 @@ namespace Hearth;
 abstract class Target
 {
 
+    /**
+     * @var array The registry of task calls
+     */
+    private $_taskRegistry = array();
+
+    /**
+     * main
+     *
+     * @access public
+     * @return void
+     */
     public function main();
 
     /**
      * task
      *
-     * Calls a task
+     * Adds a task to the registry to be built on a target
      *
      * @access public
      * @param string $task
@@ -38,7 +49,48 @@ abstract class Target
      */
     public function task($task)
     {
+        $this->addTaskToRegistry($task);
 
+        return $this;
+    }
+
+    /**
+     * getTaskRegistry
+     *
+     * Gets the task registry for the target
+     *
+     * @access public
+     * @return array
+     */
+    public function getTaskRegistry()
+    {
+        return $this->_taskRegistry;
+    }
+
+    /**
+     * addTaskToRegistry
+     *
+     * Adds a given task to the task registry
+     *
+     * @access public
+     * @param string $task
+     * @return \Hearth\Target
+     * @throws \InvalidArgumentException
+     */
+    public function addTaskToRegistry($task)
+    {
+        if (!is_string($task)) {
+            throw new \InvalidArgumentException(
+                'Could not add task to registry. '
+                . 'Unexpected ' . gettype($task) . '. Expected a string'
+            );
+        }
+
+        array_push(
+            $this->_taskRegistry,
+            $task
+        );
+        
         return $this;
     }
 }
