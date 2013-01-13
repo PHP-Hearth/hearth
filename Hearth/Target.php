9c99cb66481af20ebc48bf504746c766ad91ff2f
Maxwell Vandervelde <Max@MaxVandervelde.com>
2013-01-12 22:04:54 -0600

    Cleanup tasks references
    Removing references to a task registry in target abstraction

--- Hearth/Target.php
+++ Hearth/Target.php
@@ -23,84 +23,10 @@ namespace Hearth;
 abstract class Target
 {
     /**
-     * @var array The registry of task calls
-     */
-    private $_taskRegistry = array();
-
-    /**
      * main
      *
      * @access public
      * @return void
      */
     abstract public function main();
-
-    /**
-     * task
-     *
-     * Adds a task to the registry to be built on a target
-     *
-     * @access public
-     * @param object $task
-     * @return \Hearth\Target
-     */
-    public function addTask($task)
-    {
-        if (!is_object($task)) {
-            throw new \UnexpectedValueException(
-                'Expected an object, got a ' . gettype($task)
-            );
-        }
-        $this->addTaskToRegistry($task);
-        return $task;
-    }
-
-    /**
-     * getTaskRegistry
-     *
-     * Gets the task registry for the target
-     *
-     * @access public
-     * @return array
-     */
-    public function getTaskRegistry()
-    {
-        return $this->_taskRegistry;
-    }
-
-    /**
-     * addTaskToRegistry
-     *
-     * Adds a given task to the task registry
-     *
-     * @access public
-     * @param string $task
-     * @return \Hearth\Target
-     * @throws \InvalidArgumentException
-     */
-    public function addTaskToRegistry($task)
-    {
-        array_push(
-            $this->_taskRegistry,
-            $task
-        );
-
-        return $this;
-    }
-
-    /**
-     * Execute tasks
-     *
-     * @access public
-     * @return void
-     */
-    public function execute()
-    {
-        $tasks = $this->getTaskRegistry();
-        foreach ($tasks as $task) {
-            $task->main();
-        }
-
-        return;
-    }
 }
